f5187d9688c2f29b6d985597987d4b2faf485bf7
aakvh <96314764+aakvh@users.noreply.github.com>
2022-06-16 15:05:32 -0500

    Add files via upload

---
+++ wordsolver.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import pickle
+import sys
+
+with open('wordlist', 'rb') as f:
+    lst = pickle.load(f)
+
+char = sys.argv[1].upper().split(' ')
+
+def wordsolver():
+    #char = input('give me the list;\n')
+    matched = []
+    for i in lst:
+        if all([c in i for c in char]):
+            matched.append(i)
+    for i in matched:
+        print(i.capitalize())
+
+if __name__ == '__main__':
+    wordsolver()
